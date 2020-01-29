6a2da0df2c0448255f24a66a7146dcad034b9628
dudu <retardado.tw@gmail.com>
2020-01-29 20:26:03 -0300

    botamos as habilidades

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from 'react';
-import {Container, Paper} from '@material-ui/core';
+import {Container, Grid } from '@material-ui/core';
 import Pokemon from './components/pokemon';
+import './App.css'
 
 function App() {
     return (
         <div className="App">
             <Container>
-                <Paper variant="outlined">
-                    <Pokemon idpoke='1'/>
-                    <Pokemon idpoke='14'/>
-                </Paper>
+                <Grid container direction="row" justify="center" alignItems="center">
+                <Grid item xs={4}>
+                    <Pokemon className="poke" idpoke='1'/>
+                    </Grid>
+                    <Grid item xs={4}>
+                    <Pokemon className="poke" idpoke='14'/>
+                    </Grid>
+                </Grid>
             </Container>
         </div>
     );
--- src/components/pokemon.js
+++ src/components/pokemon.js
@@ -1,13 +1,15 @@
 import React, {Component} from 'react';
-import { Paper, Avatar } from '@material-ui/core';
+import { Typography } from '@material-ui/core';
 import axios from 'axios';
+
 
 export default class Pokemon extends Component {
     constructor(props) {
         super(props);
         this.state = {
             nome: '',
-            img: ''
+            img: '',
+            abi:''
         }
     }
 
@@ -15,15 +17,17 @@ export default class Pokemon extends Component {
         let url = "https://pokeapi.co/api/v2/pokemon/" + this.props.idpoke
         axios
             .get(url)
-            .then(resp => this.setState({nome: resp.data.name, img: resp.data.sprites.front_default}))
+            .then(resp => {
+                this.setState({nome: resp.data.name, img: resp.data.sprites.front_default, abi:resp.data.abilities[0].ability.name});
+            });
         }
 
         render() {
             return (
-            <> 
-         
-                <Avatar src={this.state.img} />
-                {this.state.nome}
+            <>
+                <img alt='poke' width='180vmin;' src={this.state.img} />
+                <Typography variant="h4">{this.state.nome.toUpperCase()}</Typography>
+                <Typography variant="subtitle1">{this.state.abi.toUpperCase()}</Typography>
             </ >)
         }
     }
